ae0195257d05f37b4a6a742706e93e97df586bd6
Leo Lu <lyuholu@gmail.com>
2025-02-21 23:15:58 -0500

    feat: add box

--- client/content.js
+++ client/content.js
@@ -1,5 +1,22 @@
-document.addEventListener("dblclick", (e) => {
-    const selected = window.getSelection()?.toString() ?? "";
+const URL = "http://localhost:8080/api/v1/courses";
+const SCHOOL = "The Ohio State University";
+const DEPARTMENT = "CSE";
+const box = document.createElement("div");
 
-    console.log(selected);
+box.classList.add("box", "hidden");
+document.body.appendChild(box);
+
+document.addEventListener("dblclick", (e) => {
+    const selectedTxt = window.getSelection()?.toString() ?? "";
+
+    box.classList.remove("hidden");
+    box.textContent = DEPARTMENT + " " + selectedTxt;
+});
+
+document.body.addEventListener("click", (e) => {
+    if (e.target === box) {
+        return;
+    }
+
+    box.classList.add("hidden");
 });
