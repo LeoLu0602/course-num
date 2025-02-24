d70e0c73f364cc13dae011c23d137a0f554ba1de
Leo Lu <lyuholu@gmail.com>
2025-02-24 15:50:32 -0500

    feat: get course title from API

--- client/content.js
+++ client/content.js
@@ -6,11 +6,56 @@ const box = document.createElement("div");
 box.classList.add("box", "hidden");
 document.body.appendChild(box);
 
-document.addEventListener("dblclick", (e) => {
+document.addEventListener("dblclick", async (e) => {
     const selectedTxt = window.getSelection()?.toString() ?? "";
+    const num = Number(selectedTxt);
+
+    // Only call API and show box if selectedTxt is a number.
+    if (Number.isNaN(num)) {
+        return;
+    }
 
     box.classList.remove("hidden");
-    box.textContent = DEPARTMENT + " " + selectedTxt;
+    box.textContent = "Searching..."
+
+    /*
+        Why URLSearchParams?
+
+        URLSearchParams is a built-in JavaScript interface 
+        that makes it easier to work with query parameters in URLs. 
+        Instead of manually parsing and constructing query strings, 
+        URLSearchParams provides convenient methods to manipulate them.
+    */
+   try {
+        const res = await fetch(
+            URL + 
+            "?" + 
+            new URLSearchParams({
+                school: SCHOOL,
+                department: DEPARTMENT,
+                num
+            }).toString(),
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            }
+        );
+
+        if (!res.ok) {
+            box.textContent = "Not found";
+
+            return;
+        }
+
+
+        const { title } = await res.json();
+        
+        box.textContent = DEPARTMENT + " " + num + " " + title;
+    } catch (err) {
+        console.error("Fetching error: ", err);
+        box.textContent = "Error!"
+    }
 });
 
 document.body.addEventListener("click", (e) => {
